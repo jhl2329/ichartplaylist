5cde4d7ad68525b34f9bb0eaaf2b2899502bc5c3
Jae Lim <jaelim2329@gmail.com>
2017-07-31 18:03:06 -0500

    Get actual string of song name instead of the whole div

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def main():
 
 	soup = BeautifulSoup(html, 'html.parser')
 
-	divList = soup.find_all('div', {"class": "ichart_score2_song1"})
+	divList = soup.find_all('div', class_="ichart_score2_song1")
 	for song in divList:
-		print(song)
+		print(song.string)
 
 
 if __name__ == '__main__':
