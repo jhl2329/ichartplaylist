b4878c8d205a9a18c71949fe53f1cbdc3a6d2561
Jae Lim <jaelim2329@gmail.com>
2017-07-31 19:16:44 -0500

    Generate list object for songs and boilerplate code for YouTube Data API

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-def main():
+def getSongList():
+	"""
+	Parses through DOM of instiz song list and returns a list of songs
+
+	:return: a list of Strings in order of popularity for that day
+	"""
 
 	html = urlopen("http://www.instiz.net/iframe_ichart_score.htm")
 
 	soup = BeautifulSoup(html, 'html.parser')
 
+	songList = []
 	divList = soup.find_all('div', class_="ichart_score2_song1")
 	for song in divList:
-		print(song.string)
+		songList.append(song.string)
+	return songList
+
+def main():
+	songList = getSongList()
+	print(songList)
 
 
 if __name__ == '__main__':
