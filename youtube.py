37c4a957cbd3e8c49d6e6a7bb40f058f221eeaa7
Jae Lim <jaelim2329@gmail.com>
2017-08-01 18:27:15 -0500

    Search for songId given list of songs

--- youtube.py
+++ youtube.py
@@ -4,6 +4,8 @@ import httplib2
 import os
 import sys
 
+from songs import get_song_list
+
 from apiclient.discovery import build
 from apiclient.errors import HttpError
 from oauth2client.client import flow_from_clientsecrets
@@ -45,7 +47,37 @@ def get_authenticated_service(args):
 	return build(API_SERVICE_NAME, API_VERSION,
 		http=credentials.authorize(httplib2.Http()))
 
+def print_results(results):
+	print(results)
+
 args = argparser.parse_args()
 service = get_authenticated_service(args)
 
 ### END BOILERPLATE CODE
+
+# Remove keyword arguments that are not set
+def remove_empty_kwargs(**kwargs):
+	good_kwargs = {}
+	if kwargs is not None:
+		for key, value in kwargs.items():
+			if value:
+				good_kwargs[key] = value
+	return good_kwargs
+
+def get_song_id(service, **kwargs):
+	"""
+	Given a song name, returns most relevant videoId for song name.
+	Since song name is pulled directly from the list that has the exact song name
+	the first result is the most relevant song.
+
+	:param: dict of part, maxResults, q, type where q is title of song
+	:return: String object representing a videoId of q
+	"""
+	videoIds = []
+	kwargs = remove_empty_kwargs(**kwargs)
+	results = service.search().list(**kwargs).execute()
+	return (((results['items'])[0])['id'])['videoId']
+
+# search_by_keyword(service, part='snippet', maxResults=25, q='query', type='')
+for song in get_song_list():
+	get_song_id(service, part='snippet', maxResults=1, q=str(song), type='')
